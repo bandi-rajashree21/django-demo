efaecd2aa22da853c5e213d8801ab606cb411d28
Bandi Rajashree <bandirs2003@gmail.com>
2026-02-05 15:16:10 +0530

    chore : add redirects

--- myproject/challenges/views.py
+++ myproject/challenges/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
 
 monthly_challenges = {
     "january": "Eat no meat for entire month",
     "february": "Walk for 20min",
     "march": "Learn Django daily",
     "april": "Read 10 pages of a book",
+    "may": "Practice coding for 1 hour",
+    "june": "Drink 3 liters of water daily",
+    "july": "Wake up at 5 AM",
+    "august": "Exercise for 30 minutes",
+    "september": "Meditate for 15 minutes",
+    "october": "Avoid sugar",
+    "november": "Write a journal daily",
+    "december": "Reflect on the year and plan ahead",
 }
 
 def monthly_challenge_by_numbers(request,month):
-    return HttpResponse(month)
+    months=list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    forward_month=months[month-1]
+    return HttpResponseRedirect("/challenges/"+forward_month)
 
 def monthly_challenge(request, month):
     try:
